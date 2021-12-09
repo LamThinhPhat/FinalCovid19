094ede0066e324712e010b26d4b2275839e3116e
danhpch2802 <91962634+danhpch2802@users.noreply.github.com>
2021-12-09 13:10:12 +0700

    Update PaymentHistory.java

--- src/UserFrame/PaymentHistory.java
+++ src/UserFrame/PaymentHistory.java
@@ -12,7 +12,7 @@ public class PaymentHistory extends JPanel{
     PaymentHistory(String username) {
 
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        JLabel lbHeader = new JLabel("Manage History");
+        JLabel lbHeader = new JLabel("Payment History");
         lbHeader.setFont(Constant.HEADER_FONT);
         lbHeader.setForeground(Constant.my_white);
         add(lbHeader);
